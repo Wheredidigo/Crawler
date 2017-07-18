14be6a22dffd1dca56aae03aaaf3f4ef05ff456b
Exmortem <dconnelly@live.com>
2017-07-17 21:50:24 -0600

    Added Overlay view

---
+++ Crawler/Crawler/Gui/Views/Overlay.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Crawler.Gui.Views
+{
+    /// <summary>
+    /// Interaction logic for Overlay.xaml
+    /// </summary>
+    public partial class Overlay : UserControl
+    {
+        public Overlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
